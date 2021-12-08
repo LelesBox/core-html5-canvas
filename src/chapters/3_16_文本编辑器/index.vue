010a5e528818d63a420e6e84e0dc895445878766
leebox <li_xiaoliang@foxmail.com>
2021-12-09 01:13:30 +0800

    基础编辑器 v1

--- src/chapters/3_16_文本编辑器/index.vue
+++ src/chapters/3_16_文本编辑器/index.vue
@@ -1,36 +1,77 @@
 
 <template>
   <div>
-    <div id="readout"></div>
-    <canvas id="canvas" width="800" height="550">
-      Canvas Not Support
-    </canvas>
+    <div id="fontSelectDiv" style="padding: 20px">
+      Font:
+      <select id="fontSelect">
+        <option value="Arial">Arial</option>
+        <option value="Comic Sans" selected>Comic Sans</option>
+        <option value="Lucida Sans">Lucida Sans</option>
+        <option value="Helvetica">Helvetica</option>
+        <option value="Palatino">Palatino</option>
+      </select>
+
+      Size:
+      <select id="sizeSelect">
+        <option value="32">32 px</option>
+        <option value="48" selected>48 px</option>
+        <option value="64">64 px</option>
+        <option value="128">128 px</option>
+      </select>
+
+      Text stroke color:
+      <select id="strokeStyleSelect">
+        <option value="red">red</option>
+        <option value="green">green</option>
+        <option value="blue">blue</option>
+        <option value="orange">orange</option>
+        <option value="cornflowerblue">cornflowerblue</option>
+        <option value="goldenrod">goldenrod</option>
+        <option value="navy" selected>navy</option>
+        <option value="purple">purple</option>
+      </select>
+
+      Text fill color:
+      <select id="fillStyleSelect">
+        <option value="rgba(255,0,0,0.5)">semi-transparent red</option>
+        <option value="green">green</option>
+        <option value="rgba(0,0,255,0.5)" selected>
+          semi-transparent blue
+        </option>
+        <option value="orange">orange</option>
+        <option value="rgba(100,140,230,0.5)">
+          semi-transparent cornflowerblue
+        </option>
+        <option value="rgba(218,165,32,0.5)">semi-transparent goldenrod</option>
+        <option value="navy">navy</option>
+        <option value="purple">purple</option>
+      </select>
+    </div>
+    <canvas id="canvas" width="800" height="550"> Canvas Not Support </canvas>
   </div>
 </template>
 
 <script>
-import { run } from './run';
+import { run } from "./run";
 export default {
   mounted() {
     run();
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  #canvas {
-    position: absolute;
-    left: 0px;
-    top: 20px;
-    margin: 20px;
-    background: #ffffff;
-    border: thin inset rgba(100, 150, 230, 0.5);
-    cursor: pointer;
-  }
-  #readout {
-    margin-top: 10px;
-    margin-left: 15px;
-    color: blue;
-    user-select: none;
-  }
+#canvas {
+  left: 0px;
+  margin: 20px;
+  background: #ffffff;
+  border: thin inset rgba(100, 150, 230, 0.5);
+  cursor: pointer;
+}
+#readout {
+  margin-top: 10px;
+  margin-left: 15px;
+  color: blue;
+  user-select: none;
+}
 </style>
--- src/chapters/3_16_文本编辑器/run.ts
+++ src/chapters/3_16_文本编辑器/run.ts
@@ -5,7 +5,256 @@ function $$<T extends Element>(selector: string) {
   return document.querySelectorAll(selector) as NodeListOf<T>;
 }
 export function run() {
-  var canvas = document.getElementById('canvas') as HTMLCanvasElement
-  var context = canvas.getContext('2d')!
-  console.log('hello world')
+  var canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  var context = canvas.getContext("2d")!;
+  console.log("hello world");
+  const fontSelect = $<HTMLSelectElement>("#fontSelect")!;
+  const sizeSelect = $<HTMLSelectElement>("#sizeSelect")!;
+  const strokeStyleSelect = $<HTMLSelectElement>("#strokeStyleSelect")!;
+  const fillStyleSelect = $<HTMLSelectElement>("#fillStyleSelect")!;
+  const GRID_STROKE_STYLE = "lightgray";
+  const GRID_HORIZONTAL_SPACING = 10;
+  const GRID_VERTICAL_SPACING = 10;
+  const cursor = new TextCursor(context);
+  let line: TextLine;
+  let blinkInterval: number;
+  let drawingSurfaceImageData: ImageData;
+  const BLINK_TIME = 1000;
+  const BLINK_OFF = 300;
+
+  function windowToCanvas(x: number, y: number) {
+    const box = canvas.getBoundingClientRect();
+    return {
+      x: x - box.left * (canvas.width / box.width),
+      y: y - box.top * (canvas.height / box.height),
+    };
+  }
+
+  function saveDrawingSurface() {
+    drawingSurfaceImageData = context.getImageData(
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+  }
+
+  function setFont() {
+    context.font = sizeSelect.value + 'px ' + fontSelect.value;
+  }
+
+  function blinkCursor(x: number, y: number) {
+    clearInterval(blinkInterval);
+    blinkInterval = setInterval(() => {
+      cursor.erase(drawingSurfaceImageData);
+      setTimeout(() => {
+        if (cursor.left === x && cursor.top + cursor.getHeight() === y) {
+          cursor.draw(x, y);
+        }
+      }, 300);
+    }, 1000);
+  }
+
+  function moveCursor(x: number, y: number) {
+    cursor.erase(drawingSurfaceImageData)
+    saveDrawingSurface();
+    context.putImageData(drawingSurfaceImageData, 0, 0);
+    cursor.draw(x, y);
+    blinkCursor(x, y);
+  }
+
+  canvas.onmousedown = e => {
+    const loc = windowToCanvas(e.clientX, e.clientY);
+    let fontHeight = context.measureText('W').width;
+    fontHeight += fontHeight / 6;
+    line = new TextLine(context, loc.x, loc.y);
+    moveCursor(loc.x, loc.y);
+  }
+
+  fillStyleSelect.onchange = e => {
+    cursor.fillStyle = fillStyleSelect.value;
+    context.fillStyle = fillStyleSelect.value;
+  }
+
+  strokeStyleSelect.onchange = e => {
+    cursor.strokeStyle = strokeStyleSelect.value;
+    context.strokeStyle = strokeStyleSelect.value;
+  }
+
+  document.onkeydown = e => {
+    if (e.keyCode === 8 || e.keyCode === 13) {
+      e.preventDefault();
+    }
+    if (e.keyCode === 8) {
+      context.save();
+      line.erase(drawingSurfaceImageData);
+      line.removeCharacterBeforeCaret();
+      moveCursor(line.left + line.getWidth(), line.bottom);
+      line.draw();
+      context.restore();
+    }
+  }
+
+  document.onkeypress = e => {
+    const key = String.fromCharCode(e.which);
+    if (e.keyCode !== 8 && !e.ctrlKey && !e.metaKey) {
+      e.preventDefault();
+      context.save();
+      line.erase(drawingSurfaceImageData);
+      line.insert(key);
+      moveCursor(line.left + line.getWidth(), line.bottom);
+      context.shadowColor = 'rgba(0,0,0,.5)';
+      context.shadowOffsetX = 1;
+      context.shadowOffsetY = 1;
+      context.shadowBlur = 2;
+      line.draw();
+      context.restore();
+    }
+  }
+  
+  function drawBackground() { // Ruled paper
+    var STEP_Y = 12,
+        i = context.canvas.height;
+    
+    context.strokeStyle = 'rgba(0,0,200,0.225)';
+    context.lineWidth = 0.5;
+ 
+    context.save();
+ 
+    while(i > STEP_Y*4) {
+       context.beginPath();
+       context.moveTo(0, i);
+       context.lineTo(context.canvas.width, i);
+       context.stroke();
+       i -= STEP_Y;
+    }
+ 
+    context.strokeStyle = 'rgba(100,0,0,0.3)';
+    context.lineWidth = 1;
+ 
+    context.beginPath();
+ 
+    context.moveTo(35,0);
+    context.lineTo(35,context.canvas.height);
+    context.stroke();
+ 
+    context.restore();
+ }
+
+  fontSelect.onchange = setFont;
+  sizeSelect.onchange = setFont;
+
+  cursor.fillStyle = fillStyleSelect.value;
+  cursor.strokeStyle = strokeStyleSelect.value;
+
+  context.fillStyle = fillStyleSelect.value;
+  context.strokeStyle = strokeStyleSelect.value;
+  context.lineWidth = 2;
+  setFont();
+  drawBackground()
+  saveDrawingSurface();
+}
+
+class TextLine {
+  text = "";
+  left: number;
+  bottom: number;
+  caret = 0;
+  context: CanvasRenderingContext2D;
+  constructor(context: CanvasRenderingContext2D, x: number, y: number) {
+    this.left = x;
+    this.bottom = y;
+    this.context = context;
+  }
+
+  insert(text: string) {
+    this.text =
+      this.text.substr(0, this.caret) + text + this.text.substr(this.caret);
+    this.caret += text.length;
+  }
+
+  removeCharacterBeforeCaret() {
+    if (this.caret === 0) {
+      return;
+    }
+    this.text =
+      this.text.substring(0, this.caret - 1) + this.text.substring(this.caret);
+    this.caret--;
+  }
+
+  getWidth() {
+    return this.context.measureText(this.text).width;
+  }
+
+  getHeight() {
+    const h = this.context.measureText("W").width;
+    return h + h / 6;
+  }
+
+  draw() {
+    const { context } = this;
+    context.save();
+    context.textAlign = "start";
+    context.textBaseline = "bottom";
+    context.strokeText(this.text, this.left, this.bottom);
+    context.fillText(this.text, this.left, this.bottom);
+    context.restore();
+  }
+
+  erase(imageData: ImageData) {
+    this.context.putImageData(imageData, 0, 0);
+  }
+}
+
+class TextCursor {
+  left = 0;
+  top = 0;
+  width = 2;
+  fillStyle = "rgba(0, 0, 0, .5)";
+  strokeStyle = '';
+  context: CanvasRenderingContext2D;
+  constructor(
+    context: CanvasRenderingContext2D,
+    width?: number,
+    fillStyle?: string
+  ) {
+    this.fillStyle = fillStyle || "rgba(0, 0, 0, .5)";
+    this.width = width || 2;
+    this.left = 0;
+    this.top = 0;
+    this.context = context;
+  }
+
+  getHeight() {
+    const h = this.context.measureText("W").width;
+    return h + h / 6;
+  }
+
+  createPath() {
+    this.context.beginPath();
+    this.context.rect(this.left, this.top, this.width, this.getHeight());
+  }
+
+  draw(left: number, bottom: number) {
+    const { context } = this;
+    context.save();
+    this.left = left;
+    this.top = bottom - this.getHeight();
+    this.createPath();
+    context.fillStyle = this.fillStyle;
+    context.fill();
+    context.restore();
+  }
+
+  erase(imageData: ImageData) {
+    this.context.putImageData(
+      imageData,
+      0,
+      0,
+      this.left,
+      this.top,
+      this.width,
+      this.getHeight()
+    );
+  }
 }
